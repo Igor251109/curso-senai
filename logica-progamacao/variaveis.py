a8d7cdf0d4828490e38407a0ee11228480da1e6d
igor <igorrocha2511@gmail.com>
2026-06-12 14:59:18 -0300

    12/06

---
+++ logica-progamacao/variaveis.py
@@ -0,0 +1,68 @@
+nome = "giovani"
+stg2 = "o rato roeu a roupa do rei de Roma"
+stg3 = "paralelepipedo"
+stg4 = "espandido"
+stg5 = "coelho"
+
+idade = 24
+num2 = 45
+num3 = 29
+num4 = 99
+num5 = 790
+
+altura = 1.36
+floats = 1.900
+flots3 = 590.00
+floasts4 = 29.90
+floats5 = 87.50
+
+tem_mundial = False
+estou_aprendendo = False
+tem_copetencia = False
+estou_tentando = True
+esta_facil = False
+
+print(nome)
+print(idade)
+print(altura)
+print(tem_mundial)
+print(stg2)
+print(stg3)
+print(stg4)
+print(stg5)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(floats)
+print(flots3)
+print(floasts4)
+print(floats5)
+print(estou_aprendendo)
+print(tem_copetencia)
+print(estou_tentando)
+print(esta_facil)
+
+nome1 = input("qual seu nome?: ")
+trabalho = input("aonde voce trabalha?: ")
+
+print(nome1)
+print(trabalho)
+
+idade1 = int(input("qual sua idade?: "))
+ano_trabalho = int(input("a quantos anos você trabalha?"))
+
+print(idade1)
+print(ano_trabalho)
+
+altura3 = float(input("qual sua altura?: "))
+salario = float(input("qual seu salário?: "))
+
+print(altura3)
+print(salario)
+
+bool1 = input("digite s/n se você trabala: ")
+bool2 = input("digite s/n se você tem dinheiro: ")
+
+print(bool1)
+print(bool2)
